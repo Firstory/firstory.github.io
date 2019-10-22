2b25bb76738ec20c87badc77bf9c15e67d7df4fb
Gordon Ueng <geniusgordon@gmail.com>
2019-10-22 12:07:54 +0800

    Small fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,12 @@ function Footer() {
     <footer className={styles.container}>
       <div className={styles.title}>Make sure to follow us!</div>
       <div className={styles.row}>
-        <img src={FB} alt="FB" className={styles.socialIcon} />
-        <img src={IG} alt="IG" className={styles.socialIcon} />
+        <a href="https://www.facebook.com/firstory.inc/">
+          <img src={FB} alt="FB" className={styles.socialIcon} />
+        </a>
+        <a href="https://www.instagram.com/firstory_lab/">
+          <img src={IG} alt="IG" className={styles.socialIcon} />
+        </a>
       </div>
       <small className={styles.copyright}>
         © Firstory 2019. All Rights Reserved.
