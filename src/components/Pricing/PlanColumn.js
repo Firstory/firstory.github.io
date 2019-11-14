5de4f181b85566b1f1d1a4278f252dc11c477347
Gordon Ueng <geniusgordon@gmail.com>
2019-11-14 11:24:55 +0800

    Refactor plan column

---
+++ src/components/Pricing/PlanColumn.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import cx from 'classnames';
+import styles from './Pricing.module.css';
+import { i18n } from '../../i18n';
+import ComingSoon from './ComingSoon';
+import Available from './Available';
+import NotAvailable from './NotAvailable';
+import HotSale from './HotSale';
+import Rocket from './Rocket';
+import { plans, developing, planBits, featureList } from './constants';
+
+const planNames = {
+  FREE: '基本方案',
+  PREMIUM: '標準方案',
+  ENTERPRISE: '企業方案',
+};
+
+const limits = {
+  FREE: 1,
+  PREMIUM: 0,
+  ENTERPRISE: 0,
+};
+
+function PlanColumn({ plan, active, onPaymentClick }) {
+  const handlePaymentClick = React.useCallback(() => {
+    onPaymentClick(plan);
+  }, [plan, onPaymentClick]);
+
+  return (
+    <div
+      className={cx(styles.planBlock, {
+        [styles.planBlockPremium]: plan === plans.PREMIUM,
+        [styles.planBlockNotActive]: !active,
+      })}
+    >
+      {plan === plans.PREMIUM && (
+        <div className={styles.absoluteRow}>
+          <Rocket alt="Rocket" className={styles.rocket} />
+          <HotSale alt="Hot sale" className={styles.hotSale} />
+        </div>
+      )}
+      <div
+        className={cx(styles.planName, {
+          [styles.planTextPremium]: plan === plans.PREMIUM,
+        })}
+      >
+        {planNames[plan]}
+      </div>
+      {plan === plans.PREMIUM ? (
+        <>
+          <div className={cx(styles.planPrice, styles.planTextPremium)}>
+            NT$249 / 月
+          </div>
+          <div className={styles.planPriceDescription}>
+            年繳{'\n'}或者月繳 NT$299 / 月
+          </div>
+        </>
+      ) : (
+        <div className={styles.planPrice}>
+          {plan === plans.FREE ? '免費' : '聯絡我們'}
+        </div>
+      )}
+      <button
+        type="button"
+        className={
+          plan === plans.PREMIUM ? styles.planButtonStroke : styles.planButton
+        }
+        onClick={handlePaymentClick}
+      >
+        聯絡我們
+      </button>
+      <div
+        className={cx(styles.planMonthly, {
+          [styles.planTextPremium]: plan === plans.PREMIUM,
+        })}
+      >
+        每個月
+        <div
+          className={cx(styles.planLimit, {
+            [styles.planTextPremium]: plan === plans.PREMIUM,
+          })}
+        >
+          {limits[plan] ? `${limits[plan]} 小時` : '無限制'}
+        </div>
+        上傳時間
+      </div>
+      {Object.keys(featureList).map(f => {
+        let className;
+        let alt;
+        let Icon;
+        if (featureList[f] & planBits[plan]) {
+          if (featureList[f] & developing) {
+            className = styles.developingFeature;
+            alt = 'Coming Soon';
+            Icon = ComingSoon;
+          } else {
+            className = styles.availableFeature;
+            alt = 'Available';
+            Icon = Available;
+          }
+        } else {
+          className =
+            plan === plans.PREMIUM
+              ? styles.unavailablePremium
+              : styles.unavailableFeature;
+          alt = 'Not Available';
+          Icon = NotAvailable;
+        }
+        return (
+          <div key={f} className={cx(styles.planTextRow, className)}>
+            <Icon
+              alt={alt}
+              className={cx(styles.icon, className, {
+                [styles.iconPremium]: plan === plans.PREMIUM,
+              })}
+            />
+            <div
+              className={cx(styles.planText, className, {
+                [styles.planTextPremium]: plan === plans.PREMIUM,
+              })}
+            >
+              {i18n.zh.pricing[f]}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default PlanColumn;
--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -1,46 +1,34 @@
 import React from 'react';
 import cx from 'classnames';
 import mixpanel from 'mixpanel-browser';
-import styles from './Pricing.module.css';
-import { i18n } from '../../i18n';
+import PlanColumn from './PlanColumn';
 import ComingSoon from './ComingSoon';
 import Available from './Available';
 import NotAvailable from './NotAvailable';
 import HotSale from './HotSale';
 import Rocket from './Rocket';
-
-const free = 1;
-const premium = 2;
-const enterprise = 4;
-const developing = 8;
-const featureList = {
-  basicAnalytics: free | premium | enterprise,
-  appRecord: free | premium | enterprise,
-  personalSite: free | premium | enterprise,
-  embeddedPlayer: free | premium | enterprise,
-  customUrl: premium | enterprise,
-  removeWaterMark: premium | enterprise,
-  advancedAnalytics: premium | enterprise | developing,
-  scheduling: premium | enterprise | developing,
-  topicPublish: premium | enterprise | developing,
-  advancedSocialShare: premium | enterprise | developing,
-  paywall: premium | enterprise | developing,
-  sponsorship: premium | enterprise | developing,
-  multiMember: enterprise | developing,
-  multiShow: enterprise | developing,
-};
-
-const plans = {
-  FREE: 1,
-  PREMIUM: 2,
-  ENTERPRISE: 3,
-};
+import {
+  free,
+  premium,
+  enterprise,
+  developing,
+  featureList,
+  plans,
+} from './constants';
+import { i18n } from '../../i18n';
+import styles from './Pricing.module.css';
 
 mixpanel.init('1fa276e4e72e21867df7c429e861eecf');
 window.mixpanel = mixpanel;
 
 function Pricing() {
   const [plan, setPlan] = React.useState(plans.PREMIUM);
+
+  function handlePaymentClick(p) {
+    mixpanel.track('Interest in Payment', { plan: p });
+    alert('即將於 12 月推出！敬請期待！');
+  }
+
   return (
     <section className={styles.section}>
       <div className={styles.container}>
@@ -78,181 +66,21 @@ function Pricing() {
           </button>
         </div>
         <div className={styles.planSection}>
-          <div
-            className={cx(styles.planBlock, {
-              [styles.planBlockNotActive]: !(plan === plans.FREE),
-            })}
-          >
-            <div className={styles.planName}>基本方案</div>
-            <div className={styles.planPrice}>免費</div>
-            <button
-              type="button"
-              className={styles.planButton}
-              onClick={() => {
-                mixpanel.track('Interest in Payment', { plan: 'FREE' });
-                alert('即將於 12 月推出！敬請期待！');
-              }}
-            >
-              開始使用
-            </button>
-            <div className={styles.planMonthly}>
-              每個月 <div className={styles.planLimit}>1 小時</div>上傳時間
-            </div>
-            {Object.keys(featureList).map(f => {
-              let className;
-              let Icon;
-              let alt;
-              if (featureList[f] === (free | premium | enterprise)) {
-                className = styles.availableFeature;
-                Icon = Available;
-                alt = 'Available';
-              } else {
-                className = styles.unavailableFeature;
-                Icon = NotAvailable;
-                alt = 'NotAvailable';
-              }
-              return (
-                <div key={f} className={cx(styles.planTextRow, className)}>
-                  <Icon alt={alt} className={styles.icon} />
-                  <div className={cx(styles.planText, className)}>
-                    {i18n.zh.pricing[f]}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div
-            className={cx(styles.planBlock, styles.planBlockPremium, {
-              [styles.planBlockNotActive]: !(plan === plans.PREMIUM),
-            })}
-          >
-            <div className={styles.absoluteRow}>
-              <Rocket alt="Rocket" className={styles.rocket} />
-              <HotSale alt="Hot sale" className={styles.hotSale} />
-            </div>
-            <div className={cx(styles.planName, styles.planTextPremium)}>
-              標準方案
-            </div>
-            <div className={cx(styles.planPrice, styles.planTextPremium)}>
-              NT$249 / 月
-            </div>
-            <div className={styles.planPriceDescription}>
-              年繳{'\n'}或者月繳 NT$299 / 月
-            </div>
-            <button
-              type="button"
-              className={styles.planButtonStroke}
-              onClick={() => {
-                mixpanel.track('Interest in Payment', { plan: 'PREMIUM' });
-                alert('即將於 12 月推出！敬請期待！');
-              }}
-            >
-              開始使用
-            </button>
-            <div className={cx(styles.planMonthly, styles.planTextPremium)}>
-              每個月{' '}
-              <div className={cx(styles.planLimit, styles.planTextPremium)}>
-                無限制
-              </div>
-              上傳時間
-            </div>
-            {Object.keys(featureList).map(f => {
-              let className;
-              let Icon;
-              let alt;
-              if (
-                featureList[f] === (free | premium | enterprise) ||
-                featureList[f] === (premium | enterprise)
-              ) {
-                className = styles.availableFeature;
-                Icon = Available;
-                alt = 'Available';
-              } else if (
-                featureList[f] ===
-                (premium | enterprise | developing)
-              ) {
-                className = styles.developingFeature;
-                Icon = ComingSoon;
-                alt = 'Coming Soon';
-              } else {
-                className = styles.unavailablePremium;
-                Icon = NotAvailable;
-                alt = 'NotAvailable';
-              }
-              return (
-                <div key={f} className={cx(styles.planTextRow, className)}>
-                  <Icon
-                    alt={alt}
-                    className={cx(styles.icon, styles.iconPremium, className)}
-                  />
-
-                  <div
-                    className={cx(
-                      styles.planText,
-                      styles.planTextPremium,
-                      className,
-                    )}
-                  >
-                    {i18n.zh.pricing[f]}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div
-            className={cx(styles.planBlock, {
-              [styles.planBlockNotActive]: !(plan === plans.ENTERPRISE),
-            })}
-          >
-            <div className={styles.planName}>企業方案</div>
-            <div className={styles.planPrice}>請聯絡我們</div>
-            <button
-              type="button"
-              className={styles.planButton}
-              onClick={() => {
-                mixpanel.track('Interest in Payment', { plan: 'ENTERPRISE' });
-                alert('即將於 12 月推出！敬請期待！');
-              }}
-            >
-              聯絡我們
-            </button>
-            <div className={styles.planMonthly}>
-              每個月 <div className={styles.planLimit}>無限制</div>上傳時間
-            </div>
-            {Object.keys(featureList).map(f => {
-              let className;
-              let alt;
-              let Icon;
-              if (
-                featureList[f] === (free | premium | enterprise) ||
-                featureList[f] === (premium | enterprise) ||
-                featureList[f] === enterprise
-              ) {
-                className = styles.availableFeature;
-                alt = 'Available';
-                Icon = Available;
-              } else if (
-                featureList[f] === (premium | enterprise | developing) ||
-                featureList[f] === (enterprise | developing)
-              ) {
-                className = styles.developingFeature;
-                alt = 'Coming Soon';
-                Icon = ComingSoon;
-              } else {
-                className = styles.unavailableFeature;
-                alt = 'Not Available';
-                Icon = NotAvailable;
-              }
-              return (
-                <div key={f} className={cx(styles.planTextRow, className)}>
-                  <Icon alt={alt} className={cx(styles.icon)} />
-                  <div className={cx(styles.planText, className)}>
-                    {i18n.zh.pricing[f]}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          <PlanColumn
+            plan="FREE"
+            active={plan === plans.FREE}
+            onPaymentClick={handlePaymentClick}
+          />
+          <PlanColumn
+            plan="PREMIUM"
+            active={plan === plans.PREMIUM}
+            onPaymentClick={handlePaymentClick}
+          />
+          <PlanColumn
+            plan="ENTERPRISE"
+            active={plan === plans.ENTERPRISE}
+            onPaymentClick={handlePaymentClick}
+          />
         </div>
         <div className={styles.iconHint}>
           <ComingSoon alt="Coming Soon" className={cx(styles.icon)} />
