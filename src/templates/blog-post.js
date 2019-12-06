625d0b908b6b7ba5f896dc25050a8e5acba5c277
Gordon Ueng <geniusgordon@gmail.com>
2019-12-06 13:43:49 +0800

    Remove hr

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
 import ArticleContainer from '../components/ArticleContainer';
-import { rhythm, scale } from '../utils/typography';
+import { rhythm } from '../utils/typography';
 
 function BlogPostTemplate({ data, pageContext, location }) {
   const post = data.markdownRemark;
   const siteTitle = data.site.siteMetadata.title;
-  const { previous, next } = pageContext;
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -27,49 +26,9 @@ function BlogPostTemplate({ data, pageContext, location }) {
             >
               {post.frontmatter.title}
             </h1>
-            {/* <p
-              style={{
-                ...scale(-1 / 5),
-                display: `block`,
-                marginBottom: rhythm(1),
-              }}
-            >
-              {post.frontmatter.date}
-            </p> */}
           </header>
           <section dangerouslySetInnerHTML={{ __html: post.html }} />
-          <hr
-            style={{
-              marginBottom: rhythm(1),
-            }}
-          />
         </article>
-        <nav>
-          <ul
-            style={{
-              display: `flex`,
-              flexWrap: `wrap`,
-              justifyContent: `space-between`,
-              listStyle: `none`,
-              padding: 0,
-            }}
-          >
-            {/* <li>
-              {previous && (
-                <Link to={previous.fields.slug} rel="prev">
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={next.fields.slug} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li> */}
-          </ul>
-        </nav>
       </ArticleContainer>
     </Layout>
   );
